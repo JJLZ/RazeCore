4c60b7467ffa5386ef2fed535998d60b3b76aa09
Jose Lopez <jjlz.cocoa@gmail.com>
2020-07-18 15:27:14 -0700

    Added Networking class.

---
+++ Sources/RazeCore/Networking.swift
@@ -0,0 +1,14 @@
+//
+//  Networking.swift
+//  RazeCore
+//
+//  Created by JJLZ on 7/18/20.
+//
+
+import Foundation
+
+extension RazeCore {
+    public class Networking {
+        
+    }
+}
---
+++ Tests/RazeCoreTests/RazeNetworkingTests.swift
@@ -0,0 +1,20 @@
+//
+//  RazeNetworkingTests.swift
+//  RazeCoreTests
+//
+//  Created by JJLZ on 7/18/20.
+//
+
+import XCTest
+
+final class RazeNetworkingTests: XCTestCase {
+
+    func testExample() throws {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    static var allTests = [
+        ("testExample", testExample)
+    ]
+}
